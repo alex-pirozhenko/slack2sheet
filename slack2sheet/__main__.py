146601c4609523f50b4ef0e2f9c8bef7757a3617
alex-pirozhenko <ap@idamob.ru>
2017-06-18 12:54:34 -0700

    publishing results to sheets

--- slack2sheet/__main__.py
+++ slack2sheet/__main__.py
@@ -1,6 +1,7 @@
 from slack2sheet.web import create_app
+import sys
 
 
 if __name__ == '__main__':
-    app = create_app()
+    app = create_app(sys.argv[1], sys.argv[2])
     app.run(host="0.0.0.0", port=8080)
--- slack2sheet/web.py
+++ slack2sheet/web.py
@@ -1,12 +1,57 @@
-from flask import Flask, request
+import json
+import gspread
+from flask import Flask, request, jsonify
+from oauth2client.service_account import ServiceAccountCredentials
 
 
-def create_app():
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive'
+]
+
+
+def get_row_count(worksheet):
+    col_values = worksheet.col_values(1)
+    return len(list(filter(None, col_values)))
+
+
+active_fields = ['message_ts', 'channel', 'user', 'action_id', 'action_name', 'action_value']
+
+
+def create_app(cred_path, sheet_url):
     app = Flask("slack2sheet")
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
+    gc = gspread.authorize(credentials)
+    sheet = gc.open_by_url(sheet_url)
+    worksheet = sheet.sheet1
+    if get_row_count(worksheet) == 0:
+        print('Resizing the empty worksheet to 1x{} and inserting the header'.format(len(active_fields)))
+        worksheet.resize(rows=1, cols=len(active_fields))
+        worksheet.append_row(active_fields)
+        worksheet.delete_row(1)
 
-    @app.route("/slack/annotate/")
+    @app.route("/slack/annotate", methods=['POST'])
     def log():
-        print(request.get_data())
-        return "foobar"
+        payload = json.loads(request.form['payload'])
+        original_message = payload['original_message']
+        del payload['original_message']
+        print(payload)
+        data = {
+            'message_ts': payload['message_ts'],
+            'channel': payload['channel']['name'],
+            'channel_id': payload['channel']['id'],
+            'user': payload['user']['name'],
+            'user_id': payload['user']['id'],
+            'action_id': payload['callback_id'],
+            'action_name': payload['actions'][0]['name'],
+            'action_value': payload['actions'][0]['value'],
+        }
+        worksheet.append_row([data.get(_) for _ in active_fields])
+        # print(payload.keys())
+        original_message['attachments'].append({
+            'text': "{} answered '{}'".format(data['user'], data['action_value']),
+            'attachment_type': 'default'
+        })
+        return jsonify(original_message)
 
     return app
